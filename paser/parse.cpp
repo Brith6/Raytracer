8130dd027404c4730e9cc3dbe3b408eb7f27dca4
ToriaEmma <emmanuela.todedji@epitech.eu>
2025-05-16 23:58:41 +0100

    parse.camera

--- paser/parse.cpp
+++ paser/parse.cpp
@@ -22,3 +22,27 @@ Parser::Parser(const std::string& filename, ErrorHandler& errorHandler)
                     " - " + pe.getError());
     }
 }
+
+bool Parser::parse(Scene& scene)
+{
+    if (errors_.hasErrors())
+        return false;
+    return parseCamera(scene)
+        && parseLights(scene)
+        && parsePrimitives(scene);
+}
+
+bool Parser::parseCamera(Scene& scene)
+{
+    try {
+        const libconfig::Setting& cam = cfg_.lookup("camera");
+        Vector3 pos, look;
+        cam.lookupValue("position", pos);
+        cam.lookupValue("look_at", look);
+        scene.setCamera(Camera{pos, look});
+    } catch (const libconfig::SettingException& e) {
+        errors_.add("Camera error: " + std::string(e.what()));
+        return false;
+    }
+    return true;
+}
