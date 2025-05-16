e42c0c0deb7af2b3ca6f236649c22bf10b9f9565
Brith6 <bryann.yabi@epitech.eu>
2025-05-16 10:44:21 +0100

    add: simple main

---
+++ src/core/Main.cpp
@@ -0,0 +1,16 @@
+/*
+** EPITECH PROJECT, 2024
+** B-OOP-400-COT-4-1-raytracer-vanessa.bokove
+** File description:
+** Main.cpp
+*/
+
+#include <string>
+
+int main(int ac, char **av)
+{
+    if (ac != 2)
+        return 84;
+    std::string sceneF = av[1];
+    return 0;
+}
