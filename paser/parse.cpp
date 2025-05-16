11d6f8fe521450c8ca7f5f1876b0b8f01b18c25c
ToriaEmma <emmanuela.todedji@epitech.eu>
2025-05-16 23:57:07 +0100

    parse.cpp

--- paser/parse.cpp
+++ paser/parse.cpp
@@ -12,54 +12,13 @@
 Parser::Parser(const std::string& filename, ErrorHandler& errorHandler)
     : errors_(errorHandler)
 {
-}
-
-bool Parser::parse(Scene& scene)
-{
-}
-
-bool Parser::parseCamera(Scene& scene)
-{
-}
-
-bool Parser::parseLights(Scene& scene)
-{
-}
-
-bool Parser::parsePrimitives(Scene& scene)
-{
-    const libconfig::Setting& primitives = cfg_.lookup("primitives");
-    bool primi = false;
-
-    if (primitives.exists("spheres")) {
-        const libconfig::Setting& spheres = primitives["spheres"];
-        if (!spheres.isList()) {
-            errors_.report("Spheres must be a list");
-            return false;
-        }
-        for (int i = 0; i < spheres.getLength(); ++i) {
-            auto primitive = PrimitiveFactory::createPrimitive(spheres[i], "sphere", errors_);
-            if (primitive) {
-                scene.addPrimitive(std::move(primitive));
-                primi = true;
-            }
-        }
+    try {
+        cfg_.readFile(filename.c_str());
+    } catch (const libconfig::FileIOException&) {
+        errors_.add("Cannot read file: " + filename);
+    } catch (const libconfig::ParseException& pe) {
+        errors_.add(pe.getFile() + ":" +
+                    std::to_string(pe.getLine()) +
+                    " - " + pe.getError());
     }
-
-    if (primitives.exists("planes")) {
-        const libconfig::Setting& planes = primitives["planes"];
-        if (!planes.isList()) {
-            errors_.report("Planes must be a list");
-            return false;
-        }
-        for (int i = 0; i < planes.getLength(); ++i) {
-            auto primitive = PrimitiveFactory::createPrimitive(planes[i], "plane", errors_);
-            if (primitive) {
-                scene.addPrimitive(std::move(primitive));
-                primi = true;
-            }
-        }
-    }
-
-    return primi;
 }
