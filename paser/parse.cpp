7d86171aaac1a7b22f58a809a97a31542431a842
ToriaEmma <emmanuela.todedji@epitech.eu>
2025-05-17 00:00:31 +0100

    parselights

--- paser/parse.cpp
+++ paser/parse.cpp
@@ -46,3 +46,28 @@ bool Parser::parseCamera(Scene& scene)
     }
     return true;
 }
+
+bool Parser::parseLights(Scene& scene)
+{
+    try {
+        const libconfig::Setting& lights = cfg_.lookup("lights");
+        for (int i = 0; i < lights.getLength(); ++i) {
+            const libconfig::Setting& l = lights[i];
+            std::string type;
+            float intensity;
+            l.lookupValue("type", type);
+            l.lookupValue("intensity", intensity);
+            if (type == "ambient") {
+                scene.addLight(std::make_unique<AmbientLight>(intensity));
+            } else if (type == "directional") {
+                Vector3 dir;
+                l.lookupValue("direction", dir);
+                scene.addLight(std::make_unique<DirectionalLight>(dir, intensity));
+            }
+        }
+    } catch (const libconfig::SettingException& e) {
+        errors_.add("Lights error: " + std::string(e.what()));
+        return false;
+    }
+    return true;
+}
