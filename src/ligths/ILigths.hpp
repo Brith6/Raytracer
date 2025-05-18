0918ae55c64522b8de539aadd3ba46669edb1e9c
ToriaEmma <emmanuela.todedji@epitech.eu>
2025-05-18 09:31:07 +0100

    ILigths.hpp

--- src/ligths/ILigths.hpp
+++ src/ligths/ILigths.hpp
@@ -9,13 +9,20 @@
 #include "../core/Color.hpp"
 #include "../core/HitData.hpp"
 #include "../core/World.hpp"
+#include <map>
 
 namespace raytracer {
     class ILigths {
         public:
             ILigths() = default;
             virtual ~ILigths() = default;
-            
-            virtual bool directLight(World &world, HitData &data, Color &color) = 0;
+            virtual Color directLight(World &world, HitData &data, Color &color) = 0;
+
+            math::Point3D _borigin;
+            math::Vector3D _bdirection;
+            Color _bcolor;
+            math::Vector3D _brotation;
+            double _bdiffuse;
+            double _bambient;
     };
 }
