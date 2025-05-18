f4dcff37a5d849f16cb5ec9db5d6308133c9a849
ToriaEmma <emmanuela.todedji@epitech.eu>
2025-05-18 09:27:13 +0100

    error case

--- paser/parse.cpp
+++ paser/parse.cpp
@@ -8,36 +8,42 @@
 #include "parse.hpp"
 #include "Primitive_fact.hpp"
 #include "ErrorHandler.hpp"
+#include "Scene.hpp"
+#include <libconfig.h++>
+#include "../src/core/Color.hpp"
+#include "../src/camera/Camera.hpp"
+#include "../src/core/Math.hpp"
+#include <map>
+#include <string>
 
-raytracer::Parser::Parser(const std::string& filename, ErrorHandler& errorHandler)
-        : errors_(errorHandler)
+using libconfig::Setting;
+using math::Vector3D;
+using math::Point3D;
+using Matrix4 = std::array<std::array<double, 4>, 4>;
+
+namespace raytracer {
+
+Parser::Parser(const std::string& filename, ErrorHandler& errorHandler)
+    : errors_(errorHandler)
 {
-        try {
-            cfg_.readFile(filename.c_str());
-        } catch (const libconfig::FileIOException& fioex) {
-            errors_.report("I/O error while reading file: " + filename);
-        } catch (const libconfig::ParseException& pex) {
-            errors_.report("Parse error at " + std::string(pex.getFile()) + ":" +
-                           std::to_string(pex.getLine()) + " - " + pex.getError());
-        }
+    try {
+        cfg_.readFile(filename.c_str());
+    } catch (const libconfig::FileIOException& fioex) {
+        errors_.report("I/O error while reading file: " + filename);
+    } catch (const libconfig::ParseException& pex) {
+        errors_.report("Parse error at " + std::string(pex.getFile()) + ":" +
+                       std::to_string(pex.getLine()) + " - " + pex.getError());
+    }
 }
 
-bool raytracer::Parser::parse(Scene& scene) {
+bool Parser::parse(Scene& scene) {
     try {
-        if (!parseCamera(scene)) {
-            return false;
-        }
-        if (!parseLights(scene)) {
-            return false;
-        }
+        if (!parseCamera(scene)) return false;
+        if (!parseLights(scene)) return false;
         auto planes = parsePlanes();
         auto spheres = parseSpheres();
-        for (const auto& plane : planes) {
-            scene.addPlane(plane);
-        }
-        for (const auto& sphere : spheres) {
-            scene.addSphere(sphere);
-        }
+        for (const auto& plane : planes) scene.addPlane(plane);
+        for (const auto& sphere : spheres) scene.addSphere(sphere);
         return true;
     } catch (const libconfig::SettingException& ex) {
         errors_.report("Setting error: " + std::string(ex.what()));
@@ -45,45 +51,33 @@ bool raytracer::Parser::parse(Scene& scene) {
     }
 }
 
-bool raytracer::Parser::parseCamera(Scene& scene)
+bool Parser::parseCamera(Scene& scene)
 {
     try {
-        const libconfig::Setting& cam = cfg_.lookup("camera");
-        Vect or3 pos, look;
-        cam.lookupValue("position", pos);
-        cam.lookupValue("look_at", look);
-        scene.setCamera(Camera{pos, look});
+        const Setting& cam = cfg_.lookup("camera");
+        int width = cam["resolution"]["width"];
+        int height = cam["resolution"]["height"];
+        float px = cam["position"]["x"];
+        float py = cam["position"]["y"];
+        float pz = cam["position"]["z"];
+        float lx = cam["look_at"]["x"];
+        float ly = cam["look_at"]["y"];
+        float lz = cam["look_at"]["z"];
+        float fov;
+        cam.lookupValue("fieldOfView", fov);
+        Camera c;
+        c.width = width;
+        c.height = height;
+        c._position = Point3D{px, py, pz};
+        c.fieldOfView = fov;
+        scene.setCamera(c);
     } catch (const libconfig::SettingException& e) {
-        errors_.add("Camera error: " + std::string(e.what()));
+        errors_.report("Camera parsing error: " + std::string(e.what()));
         return false;
     }
     return true;
 }
 
-bool raytracer::Parser::parseLights(Scene& scene)
-{
-    try {
-        const libconfig::Setting& lights = cfg_.lookup("lights");
-        for (int i = 0; i < lights.getLength(); ++i) {
-            const libconfig::Setting& l = lights[i];
-            std::string type;
-            float intensity;
-            l.lookupValue("type", type);
-            l.lookupValue("intensity", intensity);
-            if (type == "ambient") {
-                scene.addLight(std::make_unique<AmbientLight>(intensity));
-            } else if (type == "directional") {
-                Vector3 dir;
-                l.lookupValue("direction", dir);
-                scene.addLight(std::make_unique<DirectionalLight>(dir, intensity));
-            }
-        }
-    } catch (const libconfig::SettingException& e) {
-        errors_.add("Lights error: " + std::string(e.what()));
-        return false;
-    }
-    return true;
-}
 
 std::vector<Plane> raytracer::Parser::parsePlanes()
 {
@@ -109,4 +103,5 @@ std::vector<Sphere> raytracer::Parser::parseSpheres()
     const libconfig::Setting& spheres_setting = cfg_.getRoot()["primitives"]["spheres"];
     spheres = PrimitiveFact::createSphere(spheres_setting, errors_);
     return spheres;
+}
 }
