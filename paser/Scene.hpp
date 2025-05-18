a9d52143a20b84e3100770257ca159df8bbefe67
ToriaEmma <emmanuela.todedji@epitech.eu>
2025-05-18 09:25:32 +0100

    add light ambiant and camera

--- paser/Scene.hpp
+++ paser/Scene.hpp
@@ -8,11 +8,14 @@
 #ifndef SCENE_HPP
     #define SCENE_HPP
 
-#include "primitives/IPrimitives.hpp"
+#include "../src/primitives/IPrimitives.hpp"
 #include "../src/ligths/ILigths.hpp"
 #include "../src/primitives/plane/Plane.hpp"
 #include "../src/primitives/sphere/Sphere.hpp"
-#include "Camera.hpp"
+#include "../src/ligths/pointLigth/PointLigth.hpp"
+#include "../src/ligths/directionalLigth/DirectionalLigth.hpp"
+#include "../src/ligths/ambientLigth/AmbientLigth.hpp"
+#include "../src/camera/Camera.hpp"
 #include <vector>
 #include <memory>
 
@@ -22,6 +25,9 @@ namespace raytracer {
         std::vector<Plane> planes;
         std::vector<Sphere> spheres;
         std::vector<ILigths> lights_;
+        std::vector<AmbientLight> _ambientLights;
+        std::vector<PointLight> _pointLights;
+        std::vector<DirectionalLight>  _directionalLights;
         Camera camera_;
 
     public:
@@ -37,12 +43,30 @@ namespace raytracer {
         void setCamera(const Camera& camera) {
             camera_ = camera;
         }
+        void addAmbientLight(AmbientLight&& l)
+        {
+             _ambientLights.push_back(std::move(l));
+        }
+        void addPointLight(PointLight&& l)
+        {
+             _pointLights.push_back(std::move(l));
+        }
+        void addDirectionalLight(DirectionalLight&& l)
+        { 
+            _directionalLights.push_back(std::move(l));
+        }
 
         const std::vector<Plane>& getPlanes() const { return planes; }
         const std::vector<Sphere>& getSpheres() const { return spheres; }
         const std::vector<ILigths>& getLights() const { return lights_; }
         const Camera& getCamera() const { return camera_; }
+        const std::vector<AmbientLight>& getAmbientLights() const { return _ambientLights;}
+        const std::vector<PointLight>& getPointLights() const { return _pointLights;}
+        const std::vector<DirectionalLight>& getDirectionalLights()const { return _directionalLights;}
+
+
     };
 }
 
 #endif
+
